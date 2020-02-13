f96fc9f47c85bb21440137ace10e96f4ae914745
Les miz <1005766994@qq.com>
2020-02-13 15:31:25 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -18,7 +18,7 @@ def numbers_and_strings():
     # Repeat variable y 5 times
     z=y*5
     # What is the length of z?
-    lengh=len(z)
+    length=len(z)
     # Concatenate variable y with string " is good"
     y+" is good"
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
